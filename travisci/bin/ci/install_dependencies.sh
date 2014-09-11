a700f2c879f6b313e01f7b839fea516cc104a9a4
Jens Radtke <swefl@fin-sn.de>
2014-09-11 15:41:46 +0200

    try to switch current pwd before calling active mq start

--- travisci/bin/ci/install_dependencies.sh
+++ travisci/bin/ci/install_dependencies.sh
@@ -6,7 +6,6 @@ tar -xzf apache-activemq-${AMQ_VERSION}-bin.tar.gz
 
 cp travisci/conf/amq/activemq.xml apache-activemq-${AMQ_VERSION}/conf/activemq.xml
 
-pwd
-dir -l ./apache-activemq-${AMQ_VERSION}/bin/linux-x86-32/
-./apache-activemq-${AMQ_VERSION}/bin/linux-x86-32/activemq start
+cd ./apache-activemq-${AMQ_VERSION}/bin/linux-x86-32/
+./activemq start
 
