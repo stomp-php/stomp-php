0a71c6eea347d393520e0bee93741c8e74bb4f45
András Rutkai <riskawarrior@live.com>
2015-08-10 10:08:42 +0200

    New Apollo interface is added.

--- src/Stomp/Protocol.php
+++ src/Stomp/Protocol.php
@@ -85,9 +85,6 @@ class Protocol
         $frame->addHeaders($headers);
         $frame->setHeader('destination', $destination);
         $this->addClientId($frame);
-        if ($durable) {
-            $frame->setHeader('persistent', 'true');
-        }
         return $frame;
     }
 
