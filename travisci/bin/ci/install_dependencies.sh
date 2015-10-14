353504f9a08ceb95b64dd78eb9eaee164f711aaf
Jens Radtke <swefl@fin-sn.de>
2015-10-14 22:53:47 +0200

    change ActiveMQ download link to archive.apache.org

--- travisci/bin/ci/install_dependencies.sh
+++ travisci/bin/ci/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 AMQ_VERSION="5.10.0"
-wget http://mirror.netcologne.de/apache.org/activemq/${AMQ_VERSION}/apache-activemq-${AMQ_VERSION}-bin.tar.gz
+wget http://archive.apache.org/dist/activemq/${AMQ_VERSION}/apache-activemq-${AMQ_VERSION}-bin.tar.gz
 tar -xzf apache-activemq-${AMQ_VERSION}-bin.tar.gz
 
 cp travisci/conf/amq/activemq.xml apache-activemq-${AMQ_VERSION}/conf/activemq.xml
