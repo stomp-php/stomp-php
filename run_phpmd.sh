cf2f3a1b03eaa898731beab370b64de8d0a86102
Markus Staab <maggus.staab@googlemail.com>
2019-03-15 10:31:52 +0100

    remove phpmd (#122)
    * remove phpmd
    
    * Delete run_phpmd.sh

--- run_phpmd.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-./vendor/bin/phpmd src text cleancode,codesize,controversial,design,naming,unusedcode
-
-# Available formats: xml, text, html.
-# Available rulesets: cleancode,codesize,controversial,design,naming,unusedcode.
