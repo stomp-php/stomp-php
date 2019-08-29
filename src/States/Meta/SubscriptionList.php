be09d33e19cb3e9144f617a4a2c1cac200a11094
Markus Staab <maggus.staab@googlemail.com>
2019-08-29 14:31:27 +0200

    added phpdocs

--- src/States/Meta/SubscriptionList.php
+++ src/States/Meta/SubscriptionList.php
@@ -56,6 +56,8 @@ class SubscriptionList implements IteratorAggregate, ArrayAccess, Countable
 
     /**
      * @inheritdoc
+     *
+     * @return Subscription[]
      */
     public function getIterator()
     {
@@ -64,6 +66,8 @@ class SubscriptionList implements IteratorAggregate, ArrayAccess, Countable
 
     /**
      * @inheritdoc
+     *
+     * @return bool
      */
     public function offsetExists($offset)
     {
@@ -72,6 +76,8 @@ class SubscriptionList implements IteratorAggregate, ArrayAccess, Countable
 
     /**
      * @inheritdoc
+     *
+     * @return Subscription
      */
     public function offsetGet($offset)
     {
