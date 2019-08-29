cea9831f1fddc8e451aac1d1b2a106d6203e5596
Markus Staab <maggus.staab@googlemail.com>
2019-08-29 14:35:01 +0200

    Update SubscriptionList.php

--- src/States/Meta/SubscriptionList.php
+++ src/States/Meta/SubscriptionList.php
@@ -57,7 +57,7 @@ class SubscriptionList implements IteratorAggregate, ArrayAccess, Countable
     /**
      * @inheritdoc
      *
-     * @return Subscription[]
+     * @return Iterator|Subscription[]
      */
     public function getIterator()
     {
