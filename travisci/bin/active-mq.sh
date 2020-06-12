e5d90f7b8705b3ad5339fa84c1723b4312ae8293
Jens Radtke <swefl@fin-sn.de>
2020-06-12 11:28:06 +0200

    #134 let test containers onlky expose port on local loopback

--- travisci/bin/active-mq.sh
+++ travisci/bin/active-mq.sh
@@ -3,4 +3,4 @@
 VERSION=$1
 CONFIG_PATH=$2
 
-docker run --name stomp-activemq -d --rm -p 61010:61010 -v ${CONFIG_PATH}/amq/activemq.xml:/opt/activemq/conf/activemq.xml rmohr/activemq:${VERSION}-alpine
+docker run --name stomp-activemq -d --rm -p 127.0.0.1:61010:61010 -v "${CONFIG_PATH}"/amq/activemq.xml:/opt/activemq/conf/activemq.xml rmohr/activemq:"${VERSION}"-alpine
--- travisci/bin/apollo-mq.sh
+++ travisci/bin/apollo-mq.sh
@@ -2,4 +2,4 @@
 
 VERSION=$1
 
-docker run --name stomp-apollomq -d --rm -p 61020:61020 finsn/stomp-apollo:${VERSION}
+docker run --name stomp-apollomq -d --rm -p 127.0.0.1:61020:61020 finsn/stomp-apollo:"${VERSION}"
