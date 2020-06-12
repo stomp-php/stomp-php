2a3a4fd254fbfb1fbe0cc516f03eb30587b8b906
Jens Radtke <swefl@fin-sn.de>
2020-06-12 11:28:06 +0200

    #134 let test containers onlky expose port on local loopback

--- travisci/bin/rabbit-mq.sh
+++ travisci/bin/rabbit-mq.sh
@@ -3,4 +3,4 @@
 VERSION=$1
 CONFIG_PATH=$2
 
-docker run --name stomp-rabbitmq --rm -d -p 61030:61030 -v ${CONFIG_PATH}/rabbit/:/etc/rabbitmq/ rabbitmq:${VERSION}
+docker run --name stomp-rabbitmq --rm -d -p 127.0.0.1:61030:61030 -v "${CONFIG_PATH}"/rabbit/:/etc/rabbitmq/ rabbitmq:"${VERSION}"
