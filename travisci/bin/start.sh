d5b863f934c38e0e93a4a2048611ea0b4816c40b
Andrew M-Y <andrejs@gmail.com>
2021-01-26 14:37:44 +0200

    Remove extra changes

--- travisci/bin/start.sh
+++ travisci/bin/start.sh
@@ -21,30 +21,5 @@ CONFIG_PATH=$(readlink -f ./travisci/conf)
 ./travisci/bin/rabbit-mq.sh "$RABBIT_VERSION" "$CONFIG_PATH"
 ./travisci/bin/apollo-mq.sh "$APLO_VERSION"
 
-function waitForService()
-{
-    ATTEMPTS=0
-    until nc -z $1 $2; do
-        printf "wait for service %s:%s\n" $1 $2
-        ((ATTEMPTS++))
-        if [ $ATTEMPTS -ge $3 ]; then
-            printf "service is not running %s:%s\n" $1 $2
-            exit 1
-        fi
-        if [ "$FORCE_EXIT" = true ]; then
-            exit;
-        fi
-
-        sleep 1
-    done
-
-    printf "service is online %s:%s\n" $1 $2
-}
-
-waitForService 127.0.0.1 61010 50
-waitForService 127.0.0.1 61020 50
-waitForService 127.0.0.1 61030 50
-waitForService 127.0.0.1 61040 50
-
 echo ""
 echo "Brokers have been started for you, stop them by running ./travisci/bin/stop.sh"
