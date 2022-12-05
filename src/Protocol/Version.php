24d2be82dfe1b8d651eb149b772257ddfd22e506
Daniel Esteve <daniel.esteve.rem@gmail.com>
2022-12-05 15:05:14 +0100

    nullable to string

--- src/Protocol/Version.php
+++ src/Protocol/Version.php
@@ -69,7 +69,7 @@ class Version
      */
     public function getProtocol($clientId, $default = 'ActiveMQ/5.11.1')
     {
-        $server = $this->frame['server'] ? trim($this->frame['server']) : $default;
+        $server = trim((string) $this->frame['server']) ?: $default;
         $version = $this->getVersion();
         if (stristr($server, 'rabbitmq') !== false) {
             return new RabbitMq($clientId, $version, $server);
